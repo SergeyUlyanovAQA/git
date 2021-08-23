0b65aca091dad4474e84afa8170c5139252cbd56
SergeyUlyanovAQA <bigbarabumchic@mail.ru>
2021-08-23 21:31:23 +0400

    added enter data

--- src/test/java/guru/qa/TestForGit.java
+++ src/test/java/guru/qa/TestForGit.java
@@ -2,6 +2,7 @@ package guru.qa;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestForGit {
@@ -10,6 +11,11 @@ public class TestForGit {
 
     void Test() {
         open("/text-box");
+        $("#userName").setValue("Alex");
+        $("#userEmail").setValue("1IT@qa.ru");
+        $("#currentAddress").setValue("Saratov");
+        $("#permanentAddress").setValue("LA");
+        $("#submit").click();
 
     }
 }
