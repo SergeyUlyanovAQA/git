af3ebf6fff087abd1e95eae840d662e8e49c146b
SergeyUlyanovAQA <bigbarabumchic@mail.ru>
2021-08-23 21:33:17 +0400

    added checks

--- src/test/java/guru/qa/TestForGit.java
+++ src/test/java/guru/qa/TestForGit.java
@@ -2,6 +2,7 @@ package guru.qa;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -16,6 +17,10 @@ public class TestForGit {
         $("#currentAddress").setValue("Saratov");
         $("#permanentAddress").setValue("LA");
         $("#submit").click();
+        $("#name").shouldHave(text ("Alex"));
+        $("#email").shouldHave(text ("1IT@qa.ru"));
+        $("#currentAddress", 1).shouldHave(text ("Saratov"));
+        $("#permanentAddress", 1).shouldHave(text ("LA"));
 
     }
 }
