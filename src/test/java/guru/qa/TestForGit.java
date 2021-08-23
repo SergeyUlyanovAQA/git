078262f00b93d5c4c455e4a9421d5bed6fa20848
SergeyUlyanovAQA <bigbarabumchic@mail.ru>
2021-08-23 21:29:53 +0400

    added open site

--- src/test/java/guru/qa/TestForGit.java
+++ src/test/java/guru/qa/TestForGit.java
@@ -2,11 +2,14 @@ package guru.qa;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.open;
+
 public class TestForGit {
 
     @Test
 
     void Test() {
+        open("/text-box");
 
     }
 }
